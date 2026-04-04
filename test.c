daa7871036676352a0d7e106bd38406b25aba3ac
Mobassir Rehman <mobassir.rehman@Mobassirs-MacBook-Pro.local>
2026-04-04 23:59:57 +0600

    first commit

---
+++ test.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    printf("Hello world!");
+    return 0;
+   
+}
+
+#include<stdio.h>
+int main(){
+    int a;
+    scanf("%d", &a);
+    printf("a = %d", a);
+    return 0;
+}
