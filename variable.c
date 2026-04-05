3725d6b742d81b33f19e9cece87463f2f88e3f43
Mobassir Rehman <mobassir.rehman@Mobassirs-MacBook-Pro.local>
2026-04-05 23:59:57 +0600

    ...

---
+++ variable.c
@@ -0,0 +1,22 @@
+/*#include<stdio.h>
+int main(){
+    int num1 = 30;
+    num1 = 50;
+    float num2 = 4.54;
+    char c = 'Z';
+    printf("%d %f.3 %c", num1, num2, c);
+    return 0;
+}
+*/
+
+//taking input from user
+#include<stdio.h>
+int main()
+{
+    int a;
+    float b;
+    char e;
+    scanf("%d %f %c", &a, &b, &e);
+    printf("%d %f %c", a, b, e);
+    return 0;
+}
